ccbb229710df8936fefc2dea1c3f174069df34a3
SyskoSarrat <ss@camoitconsulting.fr>
2024-11-21 10:22:32 +0100

    BDD MySql mise en place et fonctionnelle

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class LoginController extends AbstractController
+{
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    public function index(#[CurrentUser] ?User $user): Response
+    {
+        if (null === $user) {
+            return $this->json(['message' => 'Invalid credentials.'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $token = 'your_generated_token'; // Remplacez par un vrai token.
+        return $this->json(['token' => $token, 'user' => $user->getUserIdentifier()]);
+    }
+}
